820509b28377d03f780bfc43c9b70967d68b3cfd
sherrypan <sherrypann@outlook.com>
2015-07-22 14:22:25 +0800

    modified section2_practice-3.

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,36 +1,33 @@
-finction objectify(str){
-  var key = str.charAt[0];
+var SYMBOL_START_POS = 2;
+function objectify(str){
+  var key = str.charAt(0);
   var count = 1;
-
-  if(str.indexOf("-") > -1 ||
-    str.indexOf(";") > -1 ||
-    str.indexOf("[]") > -1
+	if(str.indexOf("-") > -1 ||
+    str.indexOf(":") > -1 ||
+    str.indexOf("[") > -1
     ){
     count = parseInt( str.slice(SYMBOL_START_POS));
-  return { name : key,summary : count};
+
+	}
+		return { name : key,summary : count};
   }
-}
 
-function count_same_elements(collection) { 
-  var SYMBOL_START_POS = 2;
-  var objResult = {}; 
-  var result = []; 
-  
-  collection.map(function(val) { 
-    return {name: objectify.key,
-      summary: objectify.summary}
-  }).forEach(function(val) { 
-  objResult[val.key] = objResult[val.key] || 0; //objResult[val.key]为真直接赋值否则赋0
-  objResult[val.key] += val.count; 
-}) 
-  for (var i in objResult) { 
-    result.push({ 
-      key: i, 
-      count: objResult[i] 
-    }); 
-  } 
+function count_same_elements(collection) {
+
+  var objResult = {};
+  var result = [];
+
+  collection.map(function(val) {
+			return objectify(val);
+  }).forEach(function(val) {
+  objResult[val.name] = objResult[val.name] || 0; //objResult[val.key]为真直接赋值否则赋0
+  objResult[val.name] += val.summary;
+})
+  for (var i in objResult) {
+    result.push({
+      name : i,
+      summary: objResult[i]
+    });
+  }
   return result;
-} 
 }
-
-
